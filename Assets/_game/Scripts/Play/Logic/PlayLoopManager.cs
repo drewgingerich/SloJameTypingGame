ffd70a42dcf791198d2af81e879a853cbd8e9a70
Drew Gingerich <gingerich93@gmail.com>
2017-11-07 12:40:42 -0800

    Rename PlayLoop to PlayLoopManager and move into Play/Logic for organizational purposes.

--- Assets/_game/Scripts/Play/Logic/PlayLoopManager.cs
+++ Assets/_game/Scripts/Play/Logic/PlayLoopManager.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayLoop {
+public class PlayLoopManager {
 
 	BeatMapReader beatMapReader;
 	BeatTimeManager beatTimeManager;
 	BeatActivityMonitor beatActivityMonitor;
 	ScoringChecker scoringChecker;
 
-	public PlayLoop (BeatMapReader beatMapReader, BeatTimeManager beatTimeManager, 
+	public PlayLoopManager (BeatMapReader beatMapReader, BeatTimeManager beatTimeManager, 
 		BeatActivityMonitor beatActivityMonitor, ScoringChecker scoringChecker) 
 	{
 		this.beatMapReader = beatMapReader;
@@ -18,7 +18,7 @@ public class PlayLoop {
 		this.scoringChecker = scoringChecker;
 	}
 
-	public void Loop (float audioTime, List<char> inputChars) {
+	public void PlayLoop (float audioTime, List<char> inputChars) {
 		beatMapReader.ReadMapUpToTime (audioTime);
 		beatTimeManager.UpdateBeatTimes (audioTime);
 		List<Beat> activeBeats = beatActivityMonitor.ReportActiveBeats ();
