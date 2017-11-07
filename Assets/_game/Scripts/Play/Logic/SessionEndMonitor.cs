de95ed955eb6da1a6d70152f355bbcd60e687f5d
Drew Gingerich <gingerich93@gmail.com>
2017-11-07 12:37:31 -0800

    Add SessionEndmonitor to end sessions when music, text, or beats end.

---
+++ Assets/_game/Scripts/Play/Logic/SessionEndMonitor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SessionEndMonitor {
+
+	public event System.Action OnEndSession;
+
+	bool lastBeatSpawned;
+	Beat mostRecentBeat;
+
+	public SessionEndMonitor (AudioSectionPlayerBehavior audioSectionPlayer, BeatMapReader mapReader, 
+		BeatSpawner spawner, TextManager textManager) 
+	{
+		audioSectionPlayer.OnEndSection += EndSession;
+		textManager.OnTextEnd += EndSession;
+		spawner.OnSpawnBeat += RegisterBeat;
+		mapReader.OnFinishMap += () => lastBeatSpawned = true;
+	}
+
+	void RegisterBeat (Beat beat) {
+		if (mostRecentBeat != null)
+			DeregisterBeat (mostRecentBeat);
+		beat.OnDestroy += EndSessionDueToLastBeat;
+	}
+
+	void DeregisterBeat (Beat beat) {
+		beat.OnDestroy -= EndSessionDueToLastBeat;
+	}
+
+	void EndSessionDueToLastBeat (Beat _) {
+		EndSession ();
+	}
+
+	void EndSession () {
+		if (OnEndSession != null)
+			OnEndSession ();
+	}
+}
